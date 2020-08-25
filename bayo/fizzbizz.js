5cdf8c475d89d7777be4513284bad7eb4c20ea11
Abayomi <mrbayus2014@gmail.com>
2020-08-25 18:19:59 +0200

    Fuzzbuzz

---
+++ bayo/fizzbizz.js
@@ -0,0 +1,21 @@
+// 25.08.2020
+// Let's play mini FizzBuzz! For any given number, if the number is:
+
+// divisible by 3, print "Fizz".
+// divisible by "5", print "Buzz".
+// divisible by both 3 and 5, print "FizzBuzz".
+// That is, if any of the above conditions apply, print the above words instead of the number.
+// Otherwise, just print the number.
+
+let number = 15;
+if (number % 3 === 0 && number % 5 == 0) {
+    console.log("FuzzBuzz"); 
+} else if ( number % 3 === 0 ) {
+    console.log("Buzz");
+} else if (number % 5 === 0) {
+    console.log("Fuzz");
+} else console.log(number);
+
+
+
+// COMPLETED..
