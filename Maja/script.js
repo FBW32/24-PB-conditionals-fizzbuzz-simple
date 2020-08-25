0c0979b4607f6aa7870d592a5d2cfe4b11f1c542
Maja <majamarek.design@gmail.com>
2020-08-25 15:06:25 +0200

    24 done

---
+++ Maja/script.js
@@ -0,0 +1,18 @@
+/* Let's play mini FizzBuzz! For any given number, if the number is:
+
+divisible by 3, print "Fizz".
+divisible by "5", print "Buzz".
+divisible by both 3 and 5, print "FizzBuzz".  */
+
+let num1 = 4;
+if (num1 % 3 == 0) {
+  console.log("Fizz");
+}
+if (num1 % 3 == 0) {
+  console.log("Bizz");
+}
+if (num1 % 3 == 0 && num1 % 3 == 0) {
+  console.log("FizzBuzz");
+} else {
+  console.log(num1);
+} //   4
