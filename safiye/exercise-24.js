25493241ba2b5241b0109cb10dc5441725a6a791
frausafiye <s_gulenc@outlook.com>
2020-08-26 12:37:48 +0200

    solve the issue

--- safiye/exercise-24.js
+++ safiye/exercise-24.js
@@ -1,10 +1,10 @@
 for (let num = 0; num <= 100; num++) {
-  if (num % 3 == 0) {
+  if (num % 3 == 0 && num % 5 == 0) {
+    console.log("FizzBuzz");
+  } else if (num % 3 == 0) {
     console.log("Fizz");
   } else if (num % 5 == 0) {
     console.log("Buzz");
-  } else if (num % 3 == 0 && num % 5 == 0) {
-    console.log("FizzBuzz");
   } else {
     console.log(num);
   }
